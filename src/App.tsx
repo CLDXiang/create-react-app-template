0250178a7cfd6eaa46659d1a045d64fe5225441f
CLDXiang <417198670@qq.com>
2020-12-24 10:18:25 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useRef, useState, useCallback } from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
